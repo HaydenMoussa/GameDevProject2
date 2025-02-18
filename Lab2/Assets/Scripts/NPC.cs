3221b625539416f3ba3ce706aa2f13ca020dfead
Fin <lockertfin@gmail.com>
2025-02-18 11:03:11 -0600

    Dialogue always starts at beginning, added story to dialogue.

--- Lab2/Assets/Scripts/NPC.cs
+++ Lab2/Assets/Scripts/NPC.cs
@@ -2,25 +2,37 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    [SerializeField] bool firstInteraction = true;
-    [SerializeField] int repeatStartPosition;
+    // It was always skipping the first dialogue and I couldn't figure it out.
+    //
+    // bool firstInteraction = true;
+    // [SerializeField] int repeatStartPosition;
+
+    // public string npcName;
+    // public DialogueAsset dialogueAsset;
+
+    // [HideInInspector]
+    // public int StartPosition {
+    //     get
+    //     {
+    //         if (firstInteraction)
+    //         {
+    //             Debug.Log("First interaction.");
+    //             firstInteraction = false;
+    //             return 1;
+    //         }
+    //         else
+    //         {
+    //             Debug.Log("Not first interaction.");
+    //             return repeatStartPosition;
+    //         }
+    //     }
+    // }
+    //
+    // So I just made it never skip it at all. -Fin
 
     public string npcName;
     public DialogueAsset dialogueAsset;
 
     [HideInInspector]
-    public int StartPosition {
-        get
-        {
-            if (firstInteraction)
-            {
-                firstInteraction = false;
-                return 0;
-            }
-            else
-            {
-                return repeatStartPosition;
-            }
-        }
-    }
+    public int StartPosition => 0;
 }
